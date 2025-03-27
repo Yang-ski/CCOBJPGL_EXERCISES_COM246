0b6100422b45bd7bbde8ff28f4070456c0613c39
Yangski <lolzgaming911tryhardninja@gmail.com>
2025-03-27 12:40:47 +0800

    u

--- MyFolder/Exercise1/src/Main.java
+++ MyFolder/Exercise1/src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;  
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello World");
+        System.out.print("Enter your username:");
+
+        Scanner loginScanner = new Scanner(System.in);
+
+        String userInput = loginScanner.next();
+
+        System.out.print("Enter your Password:");
+        String passInput = loginScanner.next();
+
+        System.out.println("Your Username: " + userInput);
+        System.out.println("Your Password: " + passInput);
     }
-}
-
-class Person {
-
-    
 }
