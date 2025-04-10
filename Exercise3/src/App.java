71913c1524b12e584c38540750b69a07e5dab913
Yangski <lolzgaming911tryhardninja@gmail.com>
2025-04-10 13:32:22 +0800

    updated

--- Exercise3/src/App.java
+++ Exercise3/src/App.java
@@ -3,10 +3,7 @@ import java.util.ArrayList;
 public class App {
 
     public static void main(String[] args) {
-/*
-    HybridSedan sedan = new HybridSedan("Honda Civic");
-    HybridPickup pickup = new HybridPickup("Toyota Tacoma");
-*/
+
     HybridVehicle car1 = new HybridSedan(null);
     car1.carName = "Honda Civic";
     HybridVehicle car2 = new HybridPickup(null);
@@ -17,14 +14,7 @@ public class App {
     cars.add(car2);
 
     CarWash wash = new CarWash();
-/*
-    ((HybridSedan) car1).disCharging();
-    ((HybridSedan) car1).disfuelling();
-    ((HybridPickup) car2).disfuelling();
-    ((HybridPickup) car2).disCharging();
-    
-    System.out.println();
-*/
+
     ((HybridSedan) car1).disCharging();
     ((HybridSedan) car1).disfuelling();
     wash.Washing(car1);
